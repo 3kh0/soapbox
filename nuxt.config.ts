ba2ac864ff64205790fb11600122f681e309c146
Echo <github@3kh0.net>
2025-11-22 09:55:03 -0500

    x

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,35 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/main.css'],
-  vite: { plugins: [ tailwindcss(), ], },
+  vite: { plugins: [ tailwindcss() ] },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/hints',
-  ]
+  ],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Soapbox News",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          name: "description",
+          content:
+            "Soapbox, bringing you unparalleled coverage of all-things Hack Club in real-time. Not affiliated with Hack Club HQ",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+        { rel: "apple-touch-icon", href: "/icon.png" },
+      ],
+    },
+  },
 })
